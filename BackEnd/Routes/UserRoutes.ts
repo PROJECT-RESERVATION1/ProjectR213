779b3087e240eaf78a2717d2281b308d3ab0c006
aminebelho21 <belhocineamine21@gmail.com>
2023-10-14 14:35:14 +0100

    password hash

--- BackEnd/Routes/UserRoutes.ts
+++ BackEnd/Routes/UserRoutes.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { getUsers, register } from "../Controllers/UserControllers";
+import { getUsers, login, register } from "../Controllers/UserControllers";
 
 const UserRoutes = express.Router();
 
 UserRoutes.get("/", getUsers);
-// UserRoutes.post("/login", login);
+UserRoutes.post("/login", login);
 UserRoutes.post("/register", register);
 // UserRoutes.post("/edit/:id", editUSer);
 // UserRoutes.get("/delete/:id", deleteUser);
