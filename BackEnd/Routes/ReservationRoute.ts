51f0e4245e072d8c913eb90f42691bdd4eb03785
arhab213 <medriadarhab@gmail.com>
2023-10-21 12:34:57 +0100

    " backend final"

--- BackEnd/Routes/ReservationRoute.ts
+++ BackEnd/Routes/ReservationRoute.ts
@@ -1,7 +1,15 @@
-import express from 'express'
-import { PutReservation,getAllReservation,getReservation } from '../Controllers/ReservationsControllers'
-const ReservationRoute= express.Router()
-ReservationRoute.get('/',getAllReservation)
-ReservationRoute.get('/detail',getReservation)
-ReservationRoute.post('/add',PutReservation)
-export default ReservationRoute
+import express from "express";
+import {
+  PutReservation,
+  getAllReservation,
+  getReservation,
+  EditResevation,
+  Delete,
+} from "../Controllers/ReservationsControllers";
+const ReservationRoute = express.Router();
+ReservationRoute.get("/", getAllReservation);
+ReservationRoute.get("/detail", getReservation);
+ReservationRoute.post("/Edit", EditResevation);
+ReservationRoute.post("/add", PutReservation);
+ReservationRoute.get("/delete", Delete);
+export default ReservationRoute;
