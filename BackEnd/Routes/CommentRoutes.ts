80259001408c83bf145eca230aae5eb83ed0d360
aminebelho21 <belhocineamine21@gmail.com>
2023-10-21 11:07:14 +0100

    delete offer w comment

--- BackEnd/Routes/CommentRoutes.ts
+++ BackEnd/Routes/CommentRoutes.ts
@@ -1,9 +1,11 @@
 import express  from "express";
-import { addComment, getComments } from "../Controllers/CommentControllers";
+import { addComment, deleteComment, editComment, getComments } from "../Controllers/CommentControllers";
 
 const CommentRoutes = express.Router();
 
 CommentRoutes.get("/", getComments)
 CommentRoutes.post("/addComment", addComment)
+CommentRoutes.post("/change", editComment)
+CommentRoutes.get("/delete/:id", deleteComment)
 
 export default CommentRoutes;
--- BackEnd/Routes/OfferRoutes.ts
+++ BackEnd/Routes/OfferRoutes.ts
@@ -1,10 +1,11 @@
 import express  from "express";
-import { addOffer, editOffer, getOffers } from "../Controllers/OffersControllers";
+import { addOffer, deleteOffer, editOffer, getOffers } from "../Controllers/OffersControllers";
 
 const OfferRoutes = express.Router();
 
 OfferRoutes.get("/", getOffers)
 OfferRoutes.post("/addOffer", addOffer)
 OfferRoutes.post("/editOffer", editOffer)
+OfferRoutes.get("/delete/:id", deleteOffer)
 
 export default OfferRoutes;
